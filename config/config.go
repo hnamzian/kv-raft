d4f06b274c934d134ff3c0a9fbfb4df511d29254
hnamzian <namazian87@gmail.com>
2024-01-09 18:55:20 -0500

    Exported config variables

--- config/config.go
+++ config/config.go
@@ -6,42 +6,42 @@ import (
 )
 
 type Config struct {
-	id       string
-	httpPort string
-	raftPort string
+	ID       string
+	HttpPort string
+	RaftPort string
 }
 
 func GetConfig() Config {
 	cfg := Config{}
 	for i, arg := range os.Args[1:] {
 		if arg == "--node-id" {
-			cfg.id = os.Args[i+2]
+			cfg.ID = os.Args[i+2]
 			i++
 			continue
 		}
 
 		if arg == "--http-port" {
-			cfg.httpPort = os.Args[i+2]
+			cfg.HttpPort = os.Args[i+2]
 			i++
 			continue
 		}
 
 		if arg == "--raft-port" {
-			cfg.raftPort = os.Args[i+2]
+			cfg.RaftPort = os.Args[i+2]
 			i++
 			continue
 		}
 	}
 
-	if cfg.id == "" {
+	if cfg.ID == "" {
 		log.Fatal("Missing required parameter: --node-id")
 	}
 
-	if cfg.raftPort == "" {
+	if cfg.RaftPort == "" {
 		log.Fatal("Missing required parameter: --raft-port")
 	}
 
-	if cfg.httpPort == "" {
+	if cfg.HttpPort == "" {
 		log.Fatal("Missing required parameter: --http-port")
 	}
 
