ece91d1ade3b72650bd6e3a9344efe0b8f42dd2d
hnamzian <namazian87@gmail.com>
2024-01-09 18:39:20 -0500

    Read configs from cli

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	id       string
+	httpPort string
+	raftPort string
+}
+
+func GetConfig() Config {
+	cfg := Config{}
+	for i, arg := range os.Args[1:] {
+		if arg == "--node-id" {
+			cfg.id = os.Args[i+2]
+			i++
+			continue
+		}
+
+		if arg == "--http-port" {
+			cfg.httpPort = os.Args[i+2]
+			i++
+			continue
+		}
+
+		if arg == "--raft-port" {
+			cfg.raftPort = os.Args[i+2]
+			i++
+			continue
+		}
+	}
+
+	if cfg.id == "" {
+		log.Fatal("Missing required parameter: --node-id")
+	}
+
+	if cfg.raftPort == "" {
+		log.Fatal("Missing required parameter: --raft-port")
+	}
+
+	if cfg.httpPort == "" {
+		log.Fatal("Missing required parameter: --http-port")
+	}
+
+	return cfg
+}
